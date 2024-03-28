7e5abd81e5a9fdc38bd93f554d53970fa15603ae
JeonghakHeo <jeonghak4142@gmail.com>
2024-03-28 22:13:44 +1100

    Passing Functions as Values

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -3,14 +3,18 @@ import { type CourseGoal as CGoal } from '../App.tsx'
 
 type CourseGoalListProps = {
   goals: CGoal[]
+  onDeleteGoal: (id: number) => void
 }
 
-export default function CourseGoalList({ goals }: CourseGoalListProps) {
+export default function CourseGoalList({
+  goals,
+  onDeleteGoal,
+}: CourseGoalListProps) {
   return (
     <ul>
       {goals.map((goal) => (
         <li key={goal.id}>
-          <CourseGoal title={goal.title}>
+          <CourseGoal title={goal.title} id={goal.id} onDelete={onDeleteGoal}>
             <p>{goal.description}</p>
           </CourseGoal>
         </li>
