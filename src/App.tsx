981d44386631f64543d6e9377a9de0192d432ec1
JeonghakHeo <jeonghak4142@gmail.com>
2024-03-30 14:44:26 +1100

    Building a Better Polymorphic Component with Generics

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Container from './Container.tsx'
 function App() {
   return (
     <main>
-      <Container as={Button} />
+      <Container as={Button}>Click me</Container>
     </main>
   )
 }
