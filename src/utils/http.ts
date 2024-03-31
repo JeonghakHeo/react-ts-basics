3f74e1667b277c6cd4e972d95cbb9d3dadece3d9
JeonghakHeo <jeonghak4142@gmail.com>
2024-04-01 10:34:17 +1100

    Building a Utility "get" Function with TypeScript

---
+++ src/utils/http.ts
@@ -0,0 +1,11 @@
+export async function get(url: string) {
+  const response = await fetch(url)
+
+  if(!response.ok) {
+    throw new Error('Failed to fetch data.')
+  }
+
+  const data = await response.json as unknown // zod, a TypeScript-first schema declaration and validation library.
+  
+  return data
+}
