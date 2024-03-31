1924cb1042c38842c62d008fedb1008ee29f8671
JeonghakHeo <jeonghak4142@gmail.com>
2024-03-31 12:48:57 +1100

    Building Another Wrapper Component (Custom Fmr Component)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import { useRef } from 'react'
+import Form from './Form'
 import Input from './Input'
+import Button from './Button'
 
 function App() {
   const input = useRef<HTMLInputElement>(null)
 
   return (
     <main>
-      <Input label='test' id='test' ref={input} />
+      <Form>
+        <Input type='text' label='Name' id='name' />
+        <Input type='number' label='Age' id='age' />
+        <p>
+          <Button>Save</Button>
+        </p>
+      </Form>
     </main>
   )
 }
